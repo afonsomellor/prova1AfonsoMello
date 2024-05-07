3c81fd5e141736aa3812b81b904ab047560d44b2
Afonso Mello Rodrigues <162352256+afonsomellor@users.noreply.github.com>
2024-05-07 20:30:29 -0300

    Add files via upload
    Arquivos referentes a prova de linguagem de programação realizada no dia 07/05/2024 - Prof. Leandro Guarino

---
+++ script.js
@@ -0,0 +1,129 @@
+//simula um banco de dados em memória
+var clientes = []
+
+//guarda o objeto que está sendo alterado
+var clienteAlterado = null
+
+function adicionar(){
+    //libera para digitar o CPF
+    document.getElementById("cpf").disabled = false
+    clienteAlterado = null
+    mostrarModal()
+    limparForm()
+}
+function alterar(cpf){
+
+    //procurar o cliente que tem o CPF clicado no alterar
+    for(let i = 0; i < clientes.length; i++){
+        let cliente = clientes[i]
+        if (cliente.cpf == cpf){
+            //achou o cliente, entao preenche o form
+            document.getElementById("nome").value = cliente.nome
+            document.getElementById("cpf").value = cliente.cpf
+            document.getElementById("telefone").value = cliente.telefone
+            clienteAlterado = cliente
+            document.getElementById("Sexodacalopsita").value = cliente.Sexodacalopsita
+            document.getElementById("Tamanhodosapato").value = cliente.Tamanhodosapato
+            clienteAlterado = cliente
+        }
+    }
+    //bloquear o cpf para nao permitir alterá-lo
+    document.getElementById("cpf").disabled = true
+    mostrarModal()
+}
+function excluir(cpf){
+    if (confirm("Você deseja realmente excluir?")){
+        for(let i=0; i < clientes.length; i++){
+            let cliente = clientes[i]
+            if (cliente.cpf == cpf){
+                //remove o elemento encontrado na posição "i"
+                clientes.splice(i, 1) 
+            }
+        }
+        exibirDados()
+    }
+}
+function mostrarModal(){
+    let containerModal = document.getElementById("container-modal")
+    containerModal.style.display = "flex"
+}
+function ocultarModal(){
+    let containerModal = document.getElementById("container-modal")
+    containerModal.style.display = "none"
+}
+function cancelar(){
+    ocultarModal()
+    limparForm()
+}
+function salvar(){
+    let nome = document.getElementById("nome").value
+    let cpf = document.getElementById("cpf").value
+    let telefone = document.getElementById("telefone").value
+    let Sexodacalopsita = document.getElementById("Sexodacalopsita").value 
+    let Tamanhodosapato = document.getElementById("Tamanhodosapato").value 
+    //se não estiver alterando ninguém, adiciona no vetor
+    if (clienteAlterado == null){
+        let cliente = {
+            "nome": nome,
+            "cpf": cpf,
+            "telefone": telefone,
+            "Sexodacalopsita": Sexodacalopsita,
+            "Tamanhodosapato": Tamanhodosapato
+
+        
+        }
+        //adiciona o objeto cliente no vetor de clientes
+        clientes.push(cliente)
+    }else{
+        clienteAlterado.nome = nome
+        clienteAlterado.cpf = cpf
+        clienteAlterado.telefone = telefone
+        clienteAlterado.Sexodacalopsita = Sexodacalopsita
+        clienteAlterado.Tamanhodosapato = Tamanhodosapato
+    }
+
+    clienteAlterado = null
+
+    //limpa o form
+    limparForm()
+
+    ocultarModal()
+
+    exibirDados()
+}
+
+function exibirDados(){
+
+    let tbody = document.querySelector("#table-customers tbody")
+
+    //antes de listar os clientes, limpa todas as linhas
+    tbody.innerHTML = ""
+
+    for(let i = 0; i < clientes.length; i++){
+        let linha = `
+        <tr>
+            <td>${clientes[i].nome}</td>
+            <td>${clientes[i].cpf}</td>
+            <td>${clientes[i].telefone}</td>
+            <td>${clientes[i].Sexodacalopsita}</td>
+            <td>${clientes[i].Tamanhodosapato}</td>            
+            <td>
+                <button onclick="alterar('${clientes[i].cpf}')">Alterar</button>
+                <button onclick="excluir('${clientes[i].cpf}')" class= "botao-excluir">Excluir</button>
+            </td>
+        </tr>`
+        
+        let tr = document.createElement("tr")
+        tr.innerHTML = linha
+
+        tbody.appendChild(tr)
+    }
+
+}
+function limparForm(){
+    document.getElementById("nome").value = ""
+    document.getElementById("cpf").value = ""
+    document.getElementById("telefone").value = ""
+    document.getElementById("Sexodacalopsita").value = ""
+    document.getElementById("Tamanhodosapato").value = ""
+}
